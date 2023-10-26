49d2af1cd0cb741130870b05f991a2d740ed35d8
Dev Patel <dev2104patel@gmail.com>
2023-10-26 19:23:41 -0300

    added the code to connect to the database and query it

--- A3/controller/controller.js
+++ A3/controller/controller.js
@@ -14,14 +14,14 @@ exports.storeProducts = async (req, res) => {
       product.availability,
     ]);
 
-    // db.query(insertQuery, [values], (err, result) => {
-    //   if (err) {
-    //     console.error("Error executing query: ", err);
-    //     res.status(500).send({ message: "Internal Server Error.", error: err });
-    //   } else {
-    //     res.json({ message: "Products stored successfully." });
-    //   }
-    // });
+    db.query(insertQuery, [values], (err, result) => {
+      if (err) {
+        console.error("Error executing query: ", err);
+        res.status(500).send({ message: "Internal Server Error.", error: err });
+      } else {
+        res.json({ message: "Products stored successfully." });
+      }
+    });
     res.status(200).json({message: "Stored successfully"});
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error.", error: error });
@@ -30,19 +30,19 @@ exports.storeProducts = async (req, res) => {
 
 exports.listProducts = async (req, res) => {
   try {
-    // db.query("SELECT * FROM products", (err, results) => {
-    //   if (err) {
-    //     console.error("Error executing query: ", err);
-    //     res.status(500).send({ message: "Internal Server Error.", error: err });
-    //   } else {
-    //     const transformedResults = results.map((product) => ({
-    //       name: product.name,
-    //       price: product.price,
-    //       availability: product.availability === 1 ? true : false,
-    //     }));
-    //     res.json({ products: transformedResults });
-    //   }
-    // });
+    db.query("SELECT * FROM products", (err, results) => {
+      if (err) {
+        console.error("Error executing query: ", err);
+        res.status(500).send({ message: "Internal Server Error.", error: err });
+      } else {
+        const transformedResults = results.map((product) => ({
+          name: product.name,
+          price: product.price,
+          availability: product.availability === 1 ? true : false,
+        }));
+        res.json({ products: transformedResults });
+      }
+    });
 
     res.status(200).json({message: "fetched successfully"});
   } catch (error) {
--- A3/db/db.js
+++ A3/db/db.js
@@ -1,3 +1,32 @@
-// write a script to connect to the database
+const mysql = require("mysql");
 
-// write a script to create a new table in the database
+const db = mysql.createConnection({
+  host: "my-ass3-db.czitenpj2hew.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "password",
+  database: "a3db",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database Connection Failed", err);
+    return;
+  }
+  console.log("Connected to the database.");
+});
+
+const createTableQuery = `CREATE TABLE IF NOT EXISTS products (
+  name VARCHAR(100),
+  price VARCHAR(100),
+  availability boolean
+)`;
+
+db.query(createTableQuery, (err) => {
+  if (err) {
+    console.error("Error creating table:", err);
+    return;
+  }
+  console.log("Table created or already exists");
+});
+
+module.exports = db;
