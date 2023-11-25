673c973959fcb3aec6797bc8b51abc0819bc1c83
Dev Patel <dev2104patel@gmail.com>
2023-11-25 18:21:14 -0400

    added the frontend for managing the groceries

--- Term_Project/frontend/src/components/GroceryCard.js
+++ Term_Project/frontend/src/components/GroceryCard.js
@@ -1,13 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { editGroceryItem, deleteGroceryItem } from '../services/GroceryServices';
+import GroceryAlertBox from './GroceryAlertBox'; // Import your alert box component
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function GroceryCard(props) {
-    const { grocery_id, name, category, status, quantity, expiry_date } = props;
+    const { grocery_id, name, category, status, quantity, expiry_date, email } = props;
+    const { getGroceryItems } = props;
+    // State variables for edit dialog
+    const [isEditDialogOpen, setEditDialogOpen] = useState(false);
+    const [editedItem, setEditedItem] = useState({
+        grocery_id: '',
+        name: '',
+        category: '',
+        quantity: '',
+        expiry_date: '',
+        status: 'To Buy',
+        email: ''
+    });
+
+    // Function to open edit dialog and set the item being edited
+    const handleEditOpen = () => {
+        console.log(editedItem);
+        setEditDialogOpen(true);
+    };
+
+    // Function to close edit dialog
+    const handleEditDialogClose = () => {
+        console.log(editedItem);
+        setEditDialogOpen(false);
+    };
+
+    const handleFormSubmit = async () => {
+        // Add your logic to submit the form data
+        console.log(editedItem);
+        await editGroceryItem(editedItem);
+        await getGroceryItems();
+        //await addGroceryItem(newItem);
+
+        // Close the alert after submitting
+        handleEditDialogClose();
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEditedItem((prevItem) => ({ ...prevItem, [name]: value }));
+    }
+
+    const handleDelete = async () => {
+        try {
+            console.log(editedItem);
+            await deleteGroceryItem(editedItem);
+            await getGroceryItems();
+            console.log("delete is called");
+            // Show a success toast
+            toast.success('Item deleted successfully', {
+                position: 'top-right',
+                autoClose: 3000, // Duration in milliseconds
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+            });
+        } catch (error) {
+            // Handle error and show an error toast if necessary
+            toast.error('Error deleting item', {
+                position: 'top-right',
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+            });
+        }
+    }
+
+    useEffect(() => {
+        setEditedItem({
+            grocery_id: grocery_id || '',
+            name: name || '',
+            category: category || '',
+            status: status || '',
+            quantity: quantity || '',
+            expiry_date: expiry_date || '',
+            email: email || '',
+        });
+    }, [])
 
     return (
         <>
-            <div className=" w-2/3 flex flex-col justify-between bg-blue-900 max-h-100px rounded-lg p-4 gap-4">
+            <div className=" w-2/3 flex flex-col justify-between bg-blue-900 shadow-gray-800 shadow-md max-h-100px rounded-lg p-4 gap-4">
                 <div className="flex gap-2 w-full justify-between">
                     <p className="text-white font-bold text-xl">{name}</p>
                     <p className="text-white font-bold  text-xl">{category}</p>
@@ -19,13 +103,33 @@ function GroceryCard(props) {
                         {expiry_date && <p className="text-white font-medium text-md">Expiry Date: {expiry_date}</p>}
                     </div>
                     <div className='flex gap-4 items-center'>
-                        <FontAwesomeIcon icon={faEdit} style={{ fontSize: '22px', color: 'white' }} />
-                        <FontAwesomeIcon icon={faTrashAlt} style={{ fontSize: '22px', color: 'white' }} />
+                        <FontAwesomeIcon
+                            icon={faEdit}
+                            style={{ fontSize: '22px', color: 'white', cursor: 'pointer' }}
+                            onClick={handleEditOpen} // Call the edit click handler
+                            className='transform hover:scale-125 transition-transform'
+                        />
+                        <FontAwesomeIcon
+                            icon={faTrashAlt}
+                            style={{ fontSize: '22px', color: 'white', cursor: 'pointer' }}
+                            onClick={handleDelete}
+                            className='transform hover:scale-125 transition-transform'
+                        />
                     </div>
                 </div>
-
-
             </div>
+
+            {/* Edit Dialog */}
+            <GroceryAlertBox
+                isAlertOpen={isEditDialogOpen}
+                handleAlertClose={handleEditDialogClose}
+                newItem={editedItem}
+                handleInputChange={handleInputChange}
+                handleFormSubmit={handleFormSubmit}
+            // Add any other necessary props for handling edits
+            />
+
+            <ToastContainer />
         </>
     );
 }
--- Term_Project/frontend/src/pages/GroceryList.js
+++ Term_Project/frontend/src/pages/GroceryList.js
@@ -1,25 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GroceryCard from '../components/GroceryCard';
-import { getGroceryByEmail } from '../services/GroceryServices';
-import { useEffect, useState } from 'react';
+import { getGroceryByEmail, addGroceryItem } from '../services/GroceryServices';
+import GroceryAlertBox from '../components/GroceryAlertBox';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function GroceryList() {
-    // Separate items into two arrays based on their status
     const [groceryData, setGroceryData] = useState([]);
     const [toBuyItems, setToBuyItems] = useState([]);
     const [boughtItems, setBoughtItems] = useState([]);
+    const [isAlertOpen, setIsAlertOpen] = useState(false);
+    const [newItem, setNewItem] = useState({
+        name: '',
+        category: '',
+        quantity: '',
+        expiry_date: '',
+        status: 'To Buy',
+        email: ''
+    });
 
     const getGroceryItems = async () => {
-        const email = "dev2104patel@gmail.com" // later will get this email from the local storage
+        const email = "dev2104patel@gmail.com"; // get this value from the local storage
         const allItems = await getGroceryByEmail(email);
         setGroceryData(allItems);
         setToBuyItems(allItems.filter((item) => item.status === 'To Buy'));
         setBoughtItems(allItems.filter((item) => item.status === 'Bought'));
     }
 
+    const handleAlertOpen = (status) => {
+        setIsAlertOpen(true);
+        setNewItem((prevItem) => ({ ...prevItem, status }));
+    }
+
+    const handleAlertClose = () => {
+        setIsAlertOpen(false);
+        setNewItem((prevItem) => ({
+            grocery_id: '',
+            name: '',
+            category: '',
+            quantity: '',
+            expiry_date: '',
+            status: 'To Buy',
+            email: prevItem.email,
+        }));
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewItem((prevItem) => ({ ...prevItem, [name]: value }));
+    }
+
+    const handleFormSubmit = async () => {
+        // Add your logic to submit the form data
+        console.log(newItem);
+        await addGroceryItem(newItem);
+
+        const updatedToBuyItems = toBuyItems.slice();
+        const updatedBoughtItems = boughtItems.slice();
+
+        if (newItem.status === 'To Buy') {
+            updatedToBuyItems.push(newItem);
+        } else if (newItem.status === 'Bought') {
+            updatedBoughtItems.push(newItem);
+        }
+
+        setToBuyItems(updatedToBuyItems);
+        setBoughtItems(updatedBoughtItems);
+
+        // Close the alert after submitting
+        handleAlertClose();
+    }
+
     useEffect(() => {
+        //const storedEmail = localStorage.getItem('user_email')
+        setNewItem((prevItem) => ({ ...prevItem, email: 'dev2104patel@gmail.com' }));
         getGroceryItems();
-        //console.log(toBuyItems);
     }, []);
 
     return (
@@ -30,27 +85,42 @@ function GroceryList() {
 
             {/* To Buy Section */}
             <div className='flex flex-col'>
-                <div className='flex justify-center'>
-                    <h2 className="text-2xl font-bold my-4">To Buy</h2>
+                <div className='flex flex-row justify-center items-center gap-5 my-4'>
+                    <h2 className="text-2xl font-bold">To Buy</h2>
+                    <button onClick={() => handleAlertOpen('To Buy')} className="shadow-md shadow-gray-400 rounded-md py-1 px-2 font-bold">
+                    <FontAwesomeIcon icon={faPlus} className="text-black transform hover:scale-150 transition-transform" />
+                    </button>
                 </div>
                 <div className="flex flex-col gap-4 items-center">
                     {toBuyItems.map((grocery) => (
-                        <GroceryCard key={grocery.grocery_id} {...grocery} />
+                        <GroceryCard key={grocery.grocery_id} {...grocery} getGroceryItems={getGroceryItems} />
                     ))}
                 </div>
             </div>
 
             {/* Bought Section */}
             <div className='flex flex-col'>
-                <div className='flex justify-center'>
-                    <h2 className="text-2xl font-bold mt-12 mb-4">Bought</h2>
+                <div className='flex flex-row justify-center items-center gap-5 mt-12 mb-4'>
+                    <h2 className="text-2xl font-bold">Bought</h2>
+                    <button onClick={() => handleAlertOpen('Bought')} className=" shadow-md shadow-gray-400 rounded-md py-1 px-2 font-bold">
+                        <FontAwesomeIcon icon={faPlus} className="text-black transform hover:scale-150 transition-transform" />
+                    </button>
                 </div>
                 <div className="flex flex-col gap-4 items-center">
                     {boughtItems.map((grocery) => (
-                        <GroceryCard key={grocery.grocery_id} {...grocery} />
+                        <GroceryCard key={grocery.grocery_id} {...grocery} getGroceryItems={getGroceryItems} />
                     ))}
                 </div>
             </div>
+
+            {/* Alert Dialog */}
+            <GroceryAlertBox
+                isAlertOpen={isAlertOpen}
+                handleAlertClose={handleAlertClose}
+                newItem={newItem}
+                handleInputChange={handleInputChange}
+                handleFormSubmit={handleFormSubmit}
+            />
         </div>
     );
 }
