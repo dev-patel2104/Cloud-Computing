5f587d261297afaa392392762af8394ea11b6c25
Dev Patel <dev2104patel@gmail.com>
2023-11-29 18:48:35 -0400

    update

--- Term_Project/backend/GroceryCRUD/GroceryEdit/index.mjs
+++ Term_Project/backend/GroceryCRUD/GroceryEdit/index.mjs
@@ -7,9 +7,10 @@ const TABLE_NAME = "GroceryData";
 
 export const handler = async (event) => {
   try {
+    let params;
     const req = JSON.parse(event.body);
 
-    if (!req.name || !req.category || !req.expiry_date || !req.quantity || !req.status || !req.email || !req.grocery_id) {
+    if (!req.name || !req.category || !req.quantity || !req.status || !req.email || !req.grocery_id) {
       return {
         statusCode: 400,
         body: JSON.stringify({ error: "Not all the available data is present" }),
@@ -24,18 +25,35 @@ export const handler = async (event) => {
     const email = req.email;
     const grocery_id = req.grocery_id;
 
-    const params = {
-      TableName: TABLE_NAME,
-      Item: {
-        email: { S: email },
-        grocery_id: { S: grocery_id },
-        name: { S: name },
-        category: { S: category },
-        quantity: { S: quantity },
-        status: { S: status },
-        expiry_date: { N: date.toString() },
-      },
-    };
+
+    if (date) {
+      params = {
+        TableName: TABLE_NAME,
+        Item: {
+          email: { S: email },
+          grocery_id: { S: grocery_id },
+          name: { S: name },
+          category: { S: category },
+          quantity: { S: quantity },
+          status: { S: status },
+          expiry_date: { N: date.toString() },
+        },
+      };
+    }
+    else {
+      params = {
+        TableName: TABLE_NAME,
+        Item: {
+          email: { S: email },
+          grocery_id: { S: grocery_id },
+          name: { S: name },
+          category: { S: category },
+          quantity: { S: quantity },
+          status: { S: status },
+        },
+      };
+    }
+
 
     console.log("PutItem params:", params);
 
